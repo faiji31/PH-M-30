0cd81f8256eec12b32f5d051f464c55f6a5e2f8e
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 17:55:36 +0600

    9th commit

---
+++ filter.js
@@ -0,0 +1,4 @@
+const numbers =[1,2,13,14,25,6,7]
+
+const greterThan10 = numbers.filter(x => x>10)
+console.log(greterThan10)
---
+++ foreach.js
@@ -0,0 +1,2 @@
+const numbers = [1,3,5,6,7]
+numbers.forEach(x => console.log(x))
