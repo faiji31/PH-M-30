c99c2462fc56b06e76de127a282c2124c6928fa8
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 13:29:34 +0600

    4th commit

--- chain.js
+++ chain.js
@@ -1,10 +1,13 @@
 const student ={
     name:'liam',
     district:{
-        name1:'patuakhali'
+        name1:'patuakhali',
+        mother:{
+            name2:'lipi'
+        }
 
     },
     marks:72
 }
 
-console.log(student.district.name1)
+console.log(student.district.mother.name2)
