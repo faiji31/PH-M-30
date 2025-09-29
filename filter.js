e29476ec8514184f8d0d3b4b160a0437f23fdd1a
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 17:59:04 +0600

    10th commit

--- filter.js
+++ filter.js
@@ -1,4 +1,8 @@
 const numbers =[1,2,13,14,25,6,7]
 
 const greterThan10 = numbers.filter(x => x>10)
+const lessthan10 = numbers.filter(x=> x<10)
+const evenNumbers = numbers.filter(x=> x%2==0)
+console.log(evenNumbers)
+console.log(lessthan10)
 console.log(greterThan10)
