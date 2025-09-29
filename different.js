cae22c35062f612e91d2e44f75a0b79ae9fe42a6
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 18:15:14 +0600

    12th commit

--- different.js
+++ different.js
@@ -5,6 +5,12 @@ const students=[
 
 const studentName = students.map(student=> student.name)
 const cgpa = students.map(x=> x.cgpa)
+const goodstudents1 = students.find(x=>x.cgpa>3.30)
+
+console.log(goodstudents1)
+
+const goodstudents = students.filter(x=> x.cgpa>3.50)
+console.log(goodstudents)
 console.log(cgpa)
 
 console.log(studentName)
