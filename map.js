981384517f5636f1140bfde01a25ff4090c8e2dd
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 13:43:35 +0600

    7th commit

--- map.js
+++ map.js
@@ -5,6 +5,13 @@ const numbers=[1,2,3,4,5,6];
 // //     double.push(result)
 // }
 
-const doubleIt= x =>x*2
-const double = numbers.map(doubleIt)
+// const doubleIt= x =>x*2
+// const double = numbers.map(doubleIt)
+
+const double = numbers.map(x=>x*2)
 console.log(double)
+
+// 5 times
+
+const num = numbers.map(x=> x*5 )
+console.log(num)
